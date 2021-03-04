11a2f5351b771d23fc6b15214f747d9a0b87153a
Rebecca <graham.cali@gmail.com>
2021-03-04 00:55:45 -0600

    first fetch

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-let config = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        title: "Fantastic",
-        content: "So delicious and refreshing. I could drink 10",
-        rating: 5
-    })
-}
+// load the page & fetch data
 
-fetch("http://localhost:3000/cocktails/1/reviews", config)
- .then( res => res.json() )
- .then( res => console.log(res) )
+const baseUrl = "http://localhost:3000/"
+const cocktailsList = document.querySelector("#cocktails-list")
+
+
+    fetch(baseUrl + "cocktails")
+    .then(res => res.json())
+    .then(res => console.log(res))
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch(baseUrl + "cocktails")
+    .then(res => res.json())
+    .then(cocktailData => { 
+        cocktailData.forEach(cocktail => {
+            let image = document.createElement('image');
+            image.src = cocktail.image_url
+            cocktailsList.appendChild(image)
+        })
+    })
+
+})
+
+
+// let config = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         title: "Fantastic",
+//         content: "So delicious and refreshing. I could drink 10",
+//         rating: 5
+//     })
+// }
+
+// fetch("http://localhost:3000/cocktails/1/reviews", config)
+//  .then( res => res.json() )
+//  .then( res => console.log(res) )
